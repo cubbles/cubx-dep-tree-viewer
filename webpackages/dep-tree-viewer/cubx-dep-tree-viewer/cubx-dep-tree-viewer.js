caa600b7f081bd8a459422c55f3d3d95ecf874a4
Edwin Gamboa <edwjov92@gmail.com>
2017-01-25 09:05:31 -0500

    CUBBLES-29: Add method process DepTree zo it is usable by d3

--- webpackages/dep-tree-viewer/cubx-dep-tree-viewer/cubx-dep-tree-viewer.js
+++ webpackages/dep-tree-viewer/cubx-dep-tree-viewer/cubx-dep-tree-viewer.js
@@ -35,6 +35,31 @@
      * Manipulate an element’s local DOM when the cubbles framework is initialized and ready to work.
      */
     cubxReady: function () {
+    },
+
+    /**
+     * Parse a DependencyTree to a list usable by d3
+     * @param tree
+     * @param depList
+     * @returns {*}
+     */
+    _treeToList: function (tree, depList) {
+      var counter = depList.length;
+      depList.push({
+        id: counter,
+        parentId: tree.parent ? tree.parent.counter : '',
+        data: tree.data
+      });
+      if (tree.children.length === 0) {
+        return depList;
+      }
+      else {
+        for (var i = 0; i < tree.children.length; i++) {
+          tree.children[i].parent.counter = counter;
+          this.treeToList(tree.children[i], depList);
+        }
+        return depList;
+      }
     }
   });
 }());
